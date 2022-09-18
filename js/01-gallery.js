402378e44118aea4b45d6231837062be7866e7e9
Diana Kondratchuk <dianakondratchuk@gmail.com>
2022-09-18 12:03:25 +0300

    changed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,75 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
+// Change code below this line
+const gallery = document.querySelector('.gallery')
+const photoGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="'${description}'"
+            />
+            </a>`
+  })
+  .join(' ')
+gallery.insertAdjacentHTML('beforeend', ` ${photoGallery}`)
+const escapeCloseModal = event => {
+  if (event.code === 'Escape') {
+    window.removeEventListener('keydown', escapeCloseModal)
+    document.querySelector('.basicLightbox').remove()
+  }
+  return
+}
 
-const gallery = document.querySelector('.gallery');
-
-const createLi = galleryItems
-  .map(
-    (elem) =>
-      `<a class="gallery__link"><img src = '${elem.preview}' class="gallery__image" alt='${elem.description}'  data-secondSrc=${elem.original}'></a>`
+const clickImg = evt => {
+  evt.preventDefault()
+  window.addEventListener('keydown', escapeCloseModal)
+  if (!evt.target.classList.contains('gallery__image')) {
+    return
+  }
+  const instance = basicLightbox.create(
+    `<img  src="${evt.target.dataset.source}"></img>`
   )
-  .join('');
+  instance.show()
+}
+gallery.addEventListener('click', clickImg)
 
-gallery.innerHTML = createLi;
+console.log(galleryItems)
 
-const showBigPicture = (evn) => {
-  const imgBigSizeValue = evn.target.dataset.secondsrc;
+// import { galleryItems } from './gallery-items.js'
 
-  const instance = basicLightbox
-    .create(
-      `
-  	<img width="1400" height="900" src="${imgBigSizeValue}">
-    `,
-      {
-        onShow: (instance) => {
-          const checkFunk = (event) => {
-            if (event.code == 'Escape') {
-              instance.close(
-                document.removeEventListener('keydown', checkFunk)
-              );
-            }
-          };
-          document.addEventListener('keydown', checkFunk);
-        },
-      }
-    )
-    .show();
-};
+// const gallery = document.querySelector('.gallery')
 
-gallery.addEventListener('click', showBigPicture);
+// const createLi = galleryItems
+//   .map(
+//     elem =>
+//       `<a class="gallery__link"><img src = '${elem.preview}' class="gallery__image" alt='${elem.description}'  data-secondSrc=${elem.original}'></a>`
+//   )
+//   .join('')
+
+// gallery.innerHTML = createLi
+
+// const showBigPicture = evn => {
+//   const imgBigSizeValue = evn.target.dataset.secondsrc
+
+//   const instance = basicLightbox
+//     .create(
+//       `
+//   	<img width="1400" height="900" src="${imgBigSizeValue}">
+//     `,
+//       {
+//         onShow: instance => {
+//           const checkFunk = event => {
+//             if (event.code == 'Escape') {
+//               instance.close(document.removeEventListener('keydown', checkFunk))
+//             }
+//           }
+//           document.addEventListener('keydown', checkFunk)
+//         }
+//       }
+//     )
+//     .show()
+// }
+
+// gallery.addEventListener('click', showBigPicture)
