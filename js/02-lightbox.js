af4fe7dd6a4f0613a363b8c7c09daa8d613e7d7b
Diana Kondratchuk <dianakondratchuk@gmail.com>
2022-09-23 13:05:34 +0300

    changed task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,17 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
-
-const gallery = document.querySelector('.gallery');
-
-const createLi = galleryItems
-  .map(
-    (elem) =>
-      `<a class="gallery__item" href="${elem.original}" srs = "${elem.original}"><img src = '${elem.preview}' class="gallery__image"  title = '${elem.description}'></a>`
-  )
-  .join('');
-
-gallery.insertAdjacentHTML('beforeEnd', createLi);
+const gallery = document.querySelector('.gallery')
+const photoGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <li>
+            <a class="gallery__link" href="${original}">
+                <img
+                    class = "gallery__image"
+                    src = "${preview}"
+                    alt = "'${description}'"
+                />
+            </a>
+        </li> `
+  })
+  .join(' ')
+gallery.insertAdjacentHTML('beforeend', ` ${photoGallery}`)
 
 var lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
+  captionType: 'alt',
+  captionsData: 'alt',
+  captionDelay: 250
+})
+
+console.log(galleryItems)
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// const gallery = document.querySelector('.gallery');
+
+// const createLi = galleryItems
+//   .map(
+//     (elem) =>
+//       `<a class="gallery__item" href="${elem.original}" srs = "${elem.original}"><img src = '${elem.preview}' class="gallery__image"  title = '${elem.description}'></a>`
+//   )
+//   .join('');
+
+// gallery.insertAdjacentHTML('beforeEnd', createLi);
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+// });
