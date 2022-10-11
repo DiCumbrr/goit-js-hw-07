01bf3233f8a374de39a8fe855cb756bfd0c54b59
diana <dianakondratchuk@gmail.com>
2022-10-11 16:46:21 +0300

    fixed problem in ex-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,8 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
+
+
 const createLi = galleryItems
   .map(
     elem =>
@@ -17,68 +19,35 @@ const createLi = galleryItems
 gallery.innerHTML = createLi;
 
 const showBigPicture = evn => {
-  const imgBigSizeValue = evn.target.dataset.secondsrc;
 
+let imgBigSizeValue = evn.target.dataset.secondsrc;
+
+if(evn.target.className == 'gallery__image'){
   const instance = basicLightbox
-    .create(
-      `
-    <img width="1400" height="900" src="${imgBigSizeValue}">
-    `,
-      {
-        onShow: instance => {
-          const checkFunk = event => {
-            if (event.code == 'Escape') {
-              instance.close(document.removeEventListener('keydown', checkFunk));
-            }
-          };
-          document.addEventListener('keydown', checkFunk);
-        },
-      },
-    )
-    .show();
+    .create( `<img width="1400" height="900" src="${imgBigSizeValue}">`,
+  {
+    onShow: instance => {
+      document.addEventListener('keydown', checkFunk);
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown', checkFunk);
+    },
+  },  
+)
+
+let checkFunk = event => {
+  if (event.code == 'Escape') {
+      instance.close()
+  }
 };
 
+instance.show()
+
+}
+
+};
+
+
+
 gallery.addEventListener('click', showBigPicture);
-// -------------------------------------------------------
 
-//-Diana"s
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
-// const gallery = document.querySelector('.gallery');
-
-// const photoGallery = galleryItems
-//   .map(({ preview, original, description }) => {
-//     console.log(original);
-//     return `<a class="gallery__link" href="${original}">
-//             <img
-//                 class="gallery__image"
-//                 src="${preview}"
-//                 data-source="${original}"
-//                 alt="'${description}'"
-
-//             />
-//             </a>`;
-//   })
-//   .join('');
-// gallery.insertAdjacentHTML('beforeend', ` ${photoGallery}`);
-
-// function clickImg({ target }) {
-//   // evt.preventDefault();
-//   console.log(target.dataset.source);
-//   // if (target.nodeName !== 'IMG') {
-//   //   return;
-//   // }
-//   // const instance = basicLightbox.create(
-//   //   `<img  src="${target.dataset.source}" width ="800" height="600">`,
-//   // );
-//   // i
-//   const instance = basicLightbox
-//     .create(`<img src="${target.dataset.source}" width="1400" height="900">`, {
-//       onShow: instance => {
-//         // c
-//       },
-//     })
-//     .show();
-
-//   window.addEventListener('keydown', onEscapePress);
-// }
